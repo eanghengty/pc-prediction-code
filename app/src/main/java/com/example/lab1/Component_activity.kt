d3664fd0559913d82315f5c482c7f6512151c76d
tyz <tyz@pop-os.localdomain>
2022-04-06 20:07:34 +0700

    finish component page

---
+++ app/src/main/java/com/example/lab1/Component_activity.kt
@@ -0,0 +1,13 @@
+package com.example.lab1
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class Component_activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.component_activity);
+        
+    }
+
+}
--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.build_activity)
+        setContentView(R.layout.new_build_activity)
+
+
+        }
     }
-}
+
